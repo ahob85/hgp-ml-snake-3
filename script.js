5b656206a10129dda9de1009762779deb11f69ce
Aaron Hobson <aaron@ahob85.com>
2021-05-17 12:56:58 -0700

    update

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 // Author:
 
-/*******************************************************************************
-                          Global UI Variables
-
-  canvasDiv, textDiv, buttonDiv
-  In the project's HTML, the divs that will contain various elements that may
-  be created in setup(). Useful for styling (e.g., keeping them all centered).
-
-  canvas
-  The p5.js canvas. This is where all the magic happens!
-
-  textP, textP2
-  This is where you will print any kind of text (e.g., the label of an image).
-
-  buttons
-  If included, these are for user interaction (e.g., training a model, inputting
-  data).
-*******************************************************************************/
-
+// Global UI Variables
 let canvasDiv;
 let canvas;
 let textDiv;
@@ -26,26 +9,7 @@ let textP2;
 let buttonDiv;
 let resetButton;
 
-/*******************************************************************************
-                          Global Game Variables
-
-  snake
-  A snake object, represented as an array of vectors. The player character!
-
-  food
-  A food object, represented as a single vector. The thing the snake eats!
-
-  resolution
-  The resolution of the canvas. Used for graphics scaling.
-
-  scaledWidth, scaledHeight
-  Values representing the scaled width and height of the canvas, after
-  resolution is taken into account.
-
-  score
-  The player's score. Eating food increases the score by 1.
-*******************************************************************************/
-
+// Global Game Variables
 let snake;
 let food;
 let resolution;
@@ -53,27 +17,9 @@ let scaledWidth;
 let scaledHeight;
 let score;
 
-/*******************************************************************************
-                            Global ML Variables
-
-  soundClassifier
-  The machine learning model we will use in this program.
-
-  isModelReady
-  Initialized to false in setup(). Set to true when the model has been loaded
-  successfully.
-*******************************************************************************/
-
+// Global ML Variables
 let soundClassifier;
 let isModelReady;
-
-/******************************************************************************
-                                  setup()
-
-  This is a built-in p5.js function that is automatically called when the
-  program starts, just before draw(). This is used for initializing global
-  variables, building the UI, and loading images, video, data, and models.
-*******************************************************************************/
 
 function setup() {
   // Build the interface
@@ -101,14 +47,6 @@ function setup() {
 
 }
 
-/******************************************************************************
-                                  draw()
-
-  This is a built-in p5.js function that is automatically called in a repeated
-  loop, just after setup(). This is used for handling animations, or running
-  anything over and over again throughout a program.
-*******************************************************************************/
-
 function draw() {
   if(isModelReady) {
     // Scale the canvas according to resolution, then refresh the background
@@ -118,14 +56,6 @@ function draw() {
     drawGameObjects();
   }
 }
-
-/******************************************************************************
-                               drawGameObjects()
-
-  A function to make draw() a little less bulky. This simply draws all the \
-  objects in the game (namely, the snake and food). It's also where you call
-  checkCommand(), immediately before drawing the snake.
-*******************************************************************************/
 
 function drawGameObjects() {
   // Check if snake is eating the food
@@ -151,23 +81,11 @@ function drawGameObjects() {
   }
 }
 
-/******************************************************************************
-                                createFood()
-
-  Creates food and places it at a random location on the (scaled) canvas.
-*******************************************************************************/
-
 function createFood() {
   let x = floor(random(scaledWidth));
   let y = floor(random(scaledHeight));
   food = createVector(x, y);
 }
-
-/******************************************************************************
-                                checkCommand()
-
-  Change the snake's direction based on the user's command.
-*******************************************************************************/
 
 function checkCommand() {
   let commandLabel = textP2.html().toLowerCase();
@@ -182,17 +100,6 @@ function checkCommand() {
   }
 }
 
-/******************************************************************************
-                                resetGame()
-
-  A callback function. When resetButton is clicked, it calls this function. We
-  can also call this function in setup() to initialize many things in the game.
-
-  Create a new snake, reset score to 0, and place the food in a random
-  location. Call loop() to make the game continue on. Finally, hide the button
-  div again.
-*******************************************************************************/
-
 function resetGame() {
   snake = new Snake();
   createFood();
@@ -202,31 +109,9 @@ function resetGame() {
   buttonDiv.style("display", "none");
 }
 
-/******************************************************************************
-                               modelReady()
-
-  A callback function. Called after the sound classifier model has been loaded.
-  It should simply classify the image (or if using webcam video, the current
-  frame) on the canvas.
-*******************************************************************************/
-
 function modelReady() {
 
 }
-
-/******************************************************************************
-                          gotResults(error, results)
-
-  This function is a callback for classify(). In other words, after our new
-  classifier model has classified the image, it should call this function next.
-
-  parameters
-  - error: If there was an error while running classify(), it should be brought
-  up here and the function shouldn't do anything else.
-  - results: The results of classify(). This will be an object we can use to
-  get some useful information, such as the predicted label of the image, as
-  well as how confident the model is about that assigned label.
-*******************************************************************************/
 
 function gotResults(error, results) {
   if(error) {
--- snake.js
+++ snake.js
@@ -1,16 +1,5 @@
 class Snake {
 
-  /*****************************************************************************
-                                  constructor()
-
-    This method is called when we write "new Snake()", as in:
-
-    let snake = new Snake();
-
-    Every new snake we create (or "construct") will have these default
-    properties and values.
-  *****************************************************************************/
-
   constructor() {
     this.body = [];
     this.body[0] = createVector(floor(scaledWidth / 2),  floor(scaledHeight /
@@ -19,20 +8,6 @@ class Snake {
     this.yDirection = 0;
   }
 
-  /*****************************************************************************
-                                    update()
-
-    The draw() function from p5.js runs in a continuous loop. To update the
-    snake's body, we can call this method each time through the draw() loop.
-
-    The snake "moves" in a peculiar pattern, where its tail (the first element
-    of the array) is removed, its head (last element) is copied, the copied
-    head moves, then is placed back on the end of the array as a new head.
-
-    If that makes absolutely no sense to you, see the project guide for
-    visuals. ^_^
-  *****************************************************************************/
-
   update() {
     let head = this.body[this.body.length - 1].copy();
     this.body.shift();
@@ -40,18 +15,6 @@ class Snake {
     head.y += this.yDirection;
     this.body.push(head);
   }
-
-  /*****************************************************************************
-                                    endGame()
-
-    The game can end in two ways: the snake touches one of the walls of the
-    canvas, or it wraps around and touches itself.
-
-    First, write code that checks if the snake is touching a wall of the
-    canvas. Then write code that checks if it is touching any parts of its body.
-
-    If the game should end, return true. Otherwise return false.
-  *****************************************************************************/
 
   endGame() {
     let head = this.body[this.body.length - 1];
@@ -67,32 +30,10 @@ class Snake {
     return false;
   }
 
-  /*****************************************************************************
-                                    grow()
-
-    This method simply makes the snake add a new head to its body, which
-    makes it longer. For our purposes, the head is the last element of the
-    array, so
-
-    this.body[this.body.length - 1]
-
-    represents the snake's head. Copy this and place it at the end of the array
-    to give the snake a new "head", making it longer.
-  *****************************************************************************/
-
   grow() {
     let head = this.body[this.body.length - 1].copy();
     this.body.push(head);
   }
-
-  /*****************************************************************************
-                                    eat(food)
-
-    This method detects if the position of the snake's head is overlapping
-    with the position of the food. If the positions overlap, the snake should
-    grow and this method should return true. Otherwise, this method should
-    return false.
-  *****************************************************************************/
 
   eat(food) {
     let head = this.body[this.body.length - 1];
@@ -104,19 +45,6 @@ class Snake {
     }
   }
 
-  /*****************************************************************************
-                                    show()
-
-    This method shows every part of the snake on the canvas.
-
-    To do so, create a for loop and draw a rectangle for each snake part, at
-    its (x, y) coordinate, with a width and height of 1 (this will be scaled by
-    our resolution multiplier). The code for drawing the rectangle will look
-    like this:
-
-    rect(this.body[i].x, this.body[i].y, 1, 1);
-  *****************************************************************************/
-
   show() {
     for(let i = 0; i < this.body.length; i++) {
       fill(0);
@@ -124,13 +52,6 @@ class Snake {
       rect(this.body[i].x, this.body[i].y, 1, 1);
     }
   }
-
-  /*****************************************************************************
-                                setDirection(x, y)
-
-    A tiny method that sets the snake's x and y directions to the arguments
-    passed into the method.
-  *****************************************************************************/
 
   setDirection(x, y) {
     this.xDirection = x;
